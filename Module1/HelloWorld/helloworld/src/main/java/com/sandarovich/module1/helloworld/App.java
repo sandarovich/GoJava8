01135c9eabafc28dc698b632c84bc8e000557885
sandarovich <sandarovich@users.noreply.github.com>
2016-01-22 09:29:30 +0200

    V.0.0.1 Initial commit

---
+++ Module1/HelloWorld/helloworld/src/main/java/com/sandarovich/module1/helloworld/App.java
@@ -0,0 +1,16 @@
+package com.sandarovich.module1.helloworld;
+
+/**
+ * @author Olexander Kolodiazhny 2016
+ * 
+ * Console based utility. 
+ * Outputs static message "Hello World!" to user console. 
+ *
+ */
+
+public class App {
+    public static void main(String[] args) {
+	String message = "Hello World!"; 
+	System.out.println(message);
+    }
+}
