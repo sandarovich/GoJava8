c751300a8cc4570e8749c9ae6180bdbd6ae56d15
sandarovich <sandarovich@users.noreply.github.com>
2016-01-25 16:59:12 +0200

    v.0.0.4 - Beta Version. For code review.

--- Module1/LongDivide/longdivide/src/main/java/com/sandarovich/module1/longdivide/LongDivide.java
+++ Module1/LongDivide/longdivide/src/main/java/com/sandarovich/module1/longdivide/LongDivide.java
@@ -6,29 +6,34 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author Olexander Kolodiazhny
  * 
  *         Module #1. Task #4 Column Divide algorithm with text output.
  *
  */
+
 public class LongDivide {
 	private static final String COLUMN_DIVIDER = "|";
 	private static final String OPERATION_DIVIDER = "---";
 	private static final String OPERATION_MINUS = "-";
+	private static final String SPACE = " ";
 	private static final String BRACKET_LEFT = "(";
 	private static final String BRACKET_RIGHT = ")";
 	private static final int DIVIDE_DEPTH = 6;
 	private static final int GUESS_LIMIT = 10;
 	private static final int MULTIPLYER = 10;
-	private static final String TAB = "    ";
-
-	private double dividen;
-	private double divider;
+	private static final String TAB = "  ";
+	private int dividen;
+	private int divider;
 	private double result = 0;
-	private double remain = -1;
-	private double diff = 0;
+	private int remain = -1;
+	private int diff;
+	private int firstDiff;
 	private int depth = 0;
+	List<String> resultStr;
 
 	public void readNumbers() {
 		while (!parseInput(readFromKeyboard())) {
@@ -65,51 +70,63 @@ public class LongDivide {
 	}
 
 	public void showResult() {
-
-		List<String> resultStr = new ArrayList<String>();
-		// first line is immutable
+		
+		resultStr = new ArrayList<String>();
 		resultStr.add(OPERATION_MINUS + dividen + " " + COLUMN_DIVIDER + divider);
-
-
 		calcRemain(dividen, divider);
-		if (remain != 0) {
-			
-		} else {
-			
-		}
-
-
-		System.out.println("Result: " + result);
-		System.out.println("Dividen: " + dividen);
-		System.out.println("Remain: " + remain);
+		resultStr.add(1, TAB + this.firstDiff + COLUMN_DIVIDER + formatResult(this.result));
+		resultStr.add(StringUtils.repeat(TAB, depth) + OPERATION_DIVIDER);
+		resultStr.add(StringUtils.repeat(TAB, depth) + String.valueOf(this.remain));
 
 		System.out.println("Results: ");
 		for (String line : resultStr) {
 			System.out.println(line);
 		}
 	}
-
+	
+	private String formatResult(double result) {
+		String format = "%." + String.valueOf(DIVIDE_DEPTH) + "f";
+		if (remain != 0 && depth == DIVIDE_DEPTH ) {
+			return BRACKET_LEFT + String.format(format, result) + BRACKET_RIGHT;
+		} 
+		return String.format(format, result);
+		
+	}
+	
 	private double calcRemain(double dividen, double divider) {
-		System.out.println("DD-DR " + dividen + "#" + divider);
+		
+		boolean ignoreOperation = false;
 		if (remain == 0 || depth >= DIVIDE_DEPTH) {
 			return remain;
 		} else {
 			if (dividen <= divider) {
 				dividen = dividen * MULTIPLYER;
 				depth++;
+			} else {
+				firstDiff = (int) divider * findGuessCoef(dividen, divider);
 			}
-			double multipl = findGuessCoef(dividen, divider);
+			int multipl = findGuessCoef(dividen, divider);
 			result += multipl / (Math.pow(MULTIPLYER, depth));
-			diff = divider * multipl;
-			remain = dividen - divider * multipl;
+			this.diff = (int) (divider * multipl);
+			if (depth == 1 && firstDiff == 0) {
+				this.firstDiff = diff;
+				ignoreOperation = true;
+			}
+			remain = (int) (dividen - divider * multipl);
 			this.dividen = remain;
-			System.out.println("result " + result);
-			System.out.println("diff " + diff);
+			
+			if (!ignoreOperation) {
+				resultStr.add(StringUtils.repeat(TAB, depth) + OPERATION_DIVIDER);
+				resultStr.add(StringUtils.repeat(TAB, depth) + SPACE + (int) dividen);
+				resultStr.add(StringUtils.repeat(TAB, depth) + OPERATION_MINUS + diff);
+			} 
+			ignoreOperation = false;
+
 			return calcRemain(this.dividen, this.divider);
 		}
 	}
 
-	private double findGuessCoef(double dividen, double divider) {
+	private int findGuessCoef(double dividen, double divider) {
 		int guessCoef = 0;
 
 		for (int i = GUESS_LIMIT; i >= 1; i--) {
