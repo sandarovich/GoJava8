43c6a0f0841853e4f201f9f6237bc1e57671d116
sandarovich <sandarovich@users.noreply.github.com>
2016-01-25 08:30:21 +0200

    v.0.0.1  Initial

---
+++ Module1/LongDivide/longdivide/src/main/java/com/sandarovich/module1/longdivide/App.java
@@ -0,0 +1,13 @@
+package com.sandarovich.module1.longdivide;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
