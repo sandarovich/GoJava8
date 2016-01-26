9b73bb05fbbec622a9353b21776f48bc9efc7771
sa <sandarovich>
2016-01-26 20:18:29 +0200

    v.0.0.4 Code Convention

--- Module1/LongDivide/longdivide/src/main/java/com/sandarovich/module1/columndivision/App.java
+++ Module1/LongDivide/longdivide/src/main/java/com/sandarovich/module1/columndivision/App.java
@@ -1,16 +1,16 @@
 package com.sandarovich.module1.columndivision;
 
 /**
- * @author Olexander Kolodiahzny 2016 
- * Module #1. Task #4 Column Divide algorithm
- * with text output
+ * @author Olexander Kolodiahzny 2016 Module #1. Task #4 Column Divide algorithm
+ *         with text output
  *
  */
+
 public class App {
-	public static void main(String[] args) {
-		ColumnDivision ld = new ColumnDivision();
-		ld.readNumbers();
-		ld.showResult();
-		
-	}
+    
+    public static void main(String[] args) {
+        ColumnDivision ld = new ColumnDivision();
+        ld.readNumbers();
+        ld.showResult();
+    }
 }
