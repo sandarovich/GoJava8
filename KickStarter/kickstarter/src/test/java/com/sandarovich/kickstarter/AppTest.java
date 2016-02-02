be1bfaf2dd7cef7d1004f1a4c4937f91fb339d91
sandarovich <sandarovich@users.noreply.github.com>
2016-02-02 11:42:35 +0200

    v.0.0.5 - testMainMenu added.

--- KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/AppTest.java
+++ KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/AppTest.java
@@ -1,5 +1,9 @@
 package com.sandarovich.kickstarter;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Olexander Kolodiazhny 2016
  *
@@ -9,5 +13,33 @@ package com.sandarovich.kickstarter;
 
 public class AppTest {
 
+    private IO io = new ConsoleIO();
+    ConsoleMock console = new ConsoleMock();
 
+    @Test
+    public void testMainMenu() {
+        //given
+        console.addIn("1");
+        KickStarter kickStarter = new KickStarter(io);
+
+        //when
+        kickStarter.start();
+
+        //then
+        assertEquals("=======================================\n" +
+                "     Kickstarter emulator v.0.0.5\n" +
+                "     by O.Kolodiazhny 2016      \n" +
+                "=======================================\n" +
+                "\"Every big journey begins with a small step\"\n" +
+                "\n" +
+                "-----------\n" +
+                "{0} Main Menu:\n" +
+                "-----------\n" +
+                "0 - Show categories\n" +
+                "1 - Exit\n" +
+                "---\n" +
+                "1\n" +
+                ">> Bye\n", console.getOut());
+
+    }
 }
