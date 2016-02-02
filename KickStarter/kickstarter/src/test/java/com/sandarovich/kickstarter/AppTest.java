2b29308d81e01e729b51ad60927806352fa4858f
sandarovich <sandarovich@users.noreply.github.com>
2016-02-02 10:58:12 +0200

    v.0.0.5 - ConsoleMock functionality

--- KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/AppTest.java
+++ KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/AppTest.java
@@ -1,13 +1,11 @@
 package com.sandarovich.kickstarter;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
 /**
  * @author Olexander Kolodiazhny 2016
  *
  */
 
 public class AppTest {
+
 
 }
