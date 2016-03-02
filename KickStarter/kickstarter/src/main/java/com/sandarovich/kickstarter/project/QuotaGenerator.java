f64e18d6eaa9a5df9fb70ff6ffb82b5757078ea2
sa <sandarovich>
2016-03-02 12:08:23 +0200

    Quoata Generator US.1.1

--- KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/project/QuotaGenerator.java
+++ KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/project/QuotaGenerator.java
@@ -2,6 +2,7 @@ package com.sandarovich.kickstarter.project;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Quata Generator.
@@ -14,9 +15,15 @@ public class QuotaGenerator {
         QUOATES = new ArrayList<String>();
         QUOATES.add("\"Every big journey begins with a small step\"");
         QUOATES.add("\"No pain, no gain\"");
+        QUOATES.add("\"Excellence accross the board \"");
+    }
+
+    public String getQuota(int quotaIndex) {
+        return (quotaIndex <= QUOATES.size()) ? QUOATES.get(quotaIndex) : QUOATES.get(0);
     }
 
     public String getQuota() {
-        return QUOATES.get(0);
+        int quotaIndex = new Random().nextInt(QUOATES.size());
+        return getQuota(quotaIndex);
     }
 }
--- KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/QuotaGeneratorTest.java
+++ KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/QuotaGeneratorTest.java
@@ -5,12 +5,34 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class QuotaGeneratorTest {
     @Test
-    public void tetsQuoteGenerate() {
+    public void testGetQuotainListByIndex() {
         QuotaGenerator qr = new QuotaGenerator();
-        assertThat("Quota test", qr.getQuota(), is("\"Every big journey begins with a small step\""));
+        assertThat("Quota test", qr.getQuota(1), is("\"No pain, no gain\""));
+
     }
+
+    @Test
+    public void testGetUnfoundQuotaByIndex() {
+        QuotaGenerator qr = new QuotaGenerator();
+        assertThat("Unfound Quota index test", qr.getQuota(7), is("\"Every big journey begins with a small step\""));
+
+    }
+
+    @Test
+    public void testRandomQuota() {
+        //given
+        QuotaGenerator qr = mock(QuotaGenerator.class);
+        when(qr.getQuota()).thenReturn("\"No pain, no gain\"");
+
+        //then
+        assertThat("Quota test", qr.getQuota(), is("\"No pain, no gain\""));
+
+    }
+
 }
