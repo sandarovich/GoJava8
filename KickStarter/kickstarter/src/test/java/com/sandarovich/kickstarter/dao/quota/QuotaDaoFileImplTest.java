99add42edc81ed9faab1afac7a5e13384a41afc8
sandarovich <sandarovich1@ukr.net>
2016-03-13 22:04:21 +0200

    Jackson support for JSON addeds

---
+++ KickStarter/kickstarter/src/test/java/com/sandarovich/kickstarter/dao/quota/QuotaDaoFileImplTest.java
@@ -0,0 +1,19 @@
+package com.sandarovich.kickstarter.dao.quota;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by sa on 13.03.16.
+ */
+
+public class QuotaDaoFileImplTest {
+
+    @Test
+    public void testFillQuotas() throws
+            IOException {
+
+
+    }
+}
