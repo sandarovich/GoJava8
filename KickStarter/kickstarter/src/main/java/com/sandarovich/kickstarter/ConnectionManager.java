586c725f2c0f3b7c298115aad18a57dacabe4c43
sa <sandarovich>
2016-03-24 09:15:54 +0200

    External posgresql implement

--- KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/ConnectionManager.java
+++ KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/ConnectionManager.java
@@ -13,11 +13,13 @@ public class ConnectionManager {
     private Connection connection;
 
     private void openConnection() throws SQLException {
-        String user = "kickstarter";
-        String pass = "777";
-        String dbName = "kickstarter";
-        String host = "localhost";
-        String url = "jdbc:postgresql://" + host + ":5432/" + dbName;
+        String user = "vlqhnrbjejpbkg";
+        String pass = "ROfMEA0Iiwh8pbuwdB58GOwM7t";
+        String dbName = "dfg7r5r65g9u67";
+        String host = "ec2-54-83-22-48.compute-1.amazonaws.com";
+        String port = "5432";
+        String url = "jdbc:postgresql://" + host + ":" + port + "/" + dbName;
+
         connection = DriverManager.getConnection(url, user, pass);
     }
 
