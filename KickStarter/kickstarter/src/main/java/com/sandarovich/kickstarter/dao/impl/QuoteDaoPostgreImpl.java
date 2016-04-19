ed8114dfe6be4f421d40d2c54d399093d22ffbcb
sa <sandarovich>
2016-04-19 11:44:09 +0300

    Hiberante in QuoteDao.

--- KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/dao/impl/QuoteDaoPostgreImpl.java
+++ KickStarter/kickstarter/src/main/java/com/sandarovich/kickstarter/dao/impl/QuoteDaoPostgreImpl.java
@@ -2,42 +2,30 @@ package com.sandarovich.kickstarter.dao.impl;
 
 import com.sandarovich.kickstarter.dao.HibernateUtil;
 import com.sandarovich.kickstarter.dao.QuoteDao;
+import com.sandarovich.kickstarter.dao.exception.DaoException;
 import com.sandarovich.kickstarter.model.Quote;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
 import org.springframework.stereotype.Repository;
+
+import java.util.Random;
 
 @Repository
 public class QuoteDaoPostgreImpl implements QuoteDao {
     @Override
     public Quote getRandomQuota() {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        return session.get(Quote.class, 2);
+        Quote quote;
+        try (Session session = sessionFactory.openSession()) {
+            int quoteCount = ((Long) session.createCriteria(Quote.class).setProjection(Projections.rowCount()).uniqueResult()).intValue();
+            int randomQuoteIndex = new Random().nextInt(quoteCount) + 1;
+            quote = session.get(Quote.class, randomQuoteIndex);
+        } catch (HibernateException e) {
+            throw new DaoException(e);
+        }
+        return quote;
     }
-
-
-//    public static final String SQL_GET_RANDOM_QUOTE = "SELECT text, author " +
-//        "FROM " +
-//        "public.quote " +
-//        "ORDER BY RANDOM() LIMIT(1);";
-//
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-//
-//
-//    @Override
-//    public Quote getRandomQuota() {
-//        Quote quote = jdbcTemplate.queryForObject(SQL_GET_RANDOM_QUOTE, new QuoteRowMapper());
-//        return quote != null ? quote : new Quote("No Author", "No text");
-//    }
-//
-//    private final class QuoteRowMapper implements RowMapper<Quote> {
-//        public Quote mapRow(ResultSet rs, int rowNum) throws SQLException {
-//            String author = rs.getString("author");
-//            String text = rs.getString("text");
-//            return new Quote(author, text);
-//        }
-//    }
 }
 
